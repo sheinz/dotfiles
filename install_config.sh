d4b3b39d7b639c5124fa5c34c8a353618789a71f
sheinz <shein@bk.ru>
2017-04-11 18:34:41 +0300

    update install script

--- install_config.sh
+++ install_config.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-
+platform=`uname`
+if [ "$platform" = "Linux" ]
+then
+    ln -is ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+elif [ "$platform" = "Darwin" ]
+then
+    ln -is ~/dotfiles/osx/tmux.conf ~/.tmux.conf
+fi
 ln -is ~/dotfiles/vim/vimrc ~/.vimrc
-ln -is ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -is ~/dotfiles/zsh/zshrc.local ~/.zshrc.local
 ln -is ~/dotfiles/powerline ~/.config/
 ln -is ~/.vim ~/.config/nvim
