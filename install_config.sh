d8062661da5ba68f444ff99e071192fe96fff9b9
sheinz <shein@bk.ru>
2017-04-11 18:23:29 +0300

    install script for dotfiles

---
+++ install_config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+ln -is ~/dotfiles/vim/vimrc ~/.vimrc
+ln -is ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -is ~/dotfiles/zsh/zshrc.local ~/.zshrc.local
+ln -is ~/dotfiles/powerline ~/.config/
+ln -is ~/.vim ~/.config/nvim
+ln -is ~/.vimrc ~/.config/nvim/init.vim
