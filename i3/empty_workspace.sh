19758b8d31c21e948e96a365d0e59976a266bc36
sheinz <shein@bk.ru>
2017-07-28 17:54:40 +0300

    i3 lock, vim shortcust, tmux

--- i3/empty_workspace.sh
+++ i3/empty_workspace.sh
@@ -2,17 +2,19 @@
 
 existing=`i3-msg -t get_workspaces | tr , '\n'| grep num| cut -d : -f 2`
 
-echo $existing
+# echo $existing
 
 for i in $(seq 1 9)
 do
     echo $i
     if [[ $existing != *$i* ]]
     then
+        if [[ "move" == $1 ]]
+        then
+            i3-msg move container to workspace $i
+        fi
         i3-msg workspace $i
         break
     fi
 done
-
-# i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
 
